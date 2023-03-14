fe9c300ab6590acad24ff7c24d87c15ca9d77d9d
taddeoneri <taddeo.neri10@gmail.com>
2023-03-14 15:58:41 +0100

    aside images

--- js/script.js
+++ js/script.js
@@ -7,9 +7,10 @@ const images = [
 ]
 
 const slider = document.querySelector('.slider');
+const aside = document.querySelector('.aside');
 
 let slide = '';
-
+let card = '';
 let index = 0;
 
 for(let i = 0; i < images.length; i++){
@@ -19,31 +20,43 @@ for(let i = 0; i < images.length; i++){
             <img src="./img/${images[i]}" alt="${i}">
         </div>
     `;
+    card += 
+    `
+        <div class="card">
+            <img src="./img/${images[i]}" alt="${i}">
+        </div>
+    `;
 }
 
 slider.innerHTML += slide;
+aside.innerHTML += card;
 
 document.querySelectorAll('.slide')[index].classList.add('active');
+document.querySelectorAll('.card')[index].classList.add('opacity');
 
 back = document.getElementById('goBack');
 next = document.getElementById('goNext');
 
 back.addEventListener('click', function(){
     document.querySelectorAll('.slide')[index].classList.remove('active');
+    document.querySelectorAll('.card')[index].classList.remove('opacity');
     if(index == 0){
         index = images.length - 1;
     } else{
         index --;
     }
     document.querySelectorAll('.slide')[index].classList.add('active');
+    document.querySelectorAll('.card')[index].classList.add('opacity');
 })
 
 next.addEventListener('click', function(){
     document.querySelectorAll('.slide')[index].classList.remove('active');
+    document.querySelectorAll('.card')[index].classList.remove('opacity');
     if(index == images.length - 1){
         index = 0;
     } else{
         index ++;
     }
     document.querySelectorAll('.slide')[index].classList.add('active');
+    document.querySelectorAll('.card')[index].classList.add('opacity');
 })
